41c34a529d39cc48ed588b71aa7ce9700f06a3ef
IBRAHIM\Ibrahin <ishaheen910@gmail.com>
2022-01-01 21:49:36 +0200

    first setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
+//external
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore,applyMiddleware } from "redux";
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+// local 
 import App from './App';
+import rootReducer from './reducers'
+//assets
+import './index.css';
 
 
+const store=createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
