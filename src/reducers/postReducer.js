57fc9d3ef5f3150343b5d90a0800c77a2758d1ad
IBRAHIM\Ibrahin <ishaheen910@gmail.com>
2022-01-01 22:33:32 +0200

    add actions to reducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,12 +1,19 @@
-export const initialState={
-    posts:[],
-    loading:false,
-    hasErrors:false
-}
-export default function postsReducer(state=initialState,action){
-    switch (action.type) {
-    
-        default:
-            return state;
-    }
+import * as actionTypes from "../types";
+export const initialState = {
+  posts: [],
+  loading: false,
+  hasErrors: false,
+};
+
+export default function postsReducer(state = initialState, action) {
+  switch (action.type) {
+    case actionTypes.GET_POSTS:
+      return { ...state, loading: true };
+    case actionTypes.GET_POSTS_SUCCESS:
+      return { posts: action.payload, loading: false, hasErrors: false };
+    case actionTypes.GET_POSTS_FAILURE:
+      return { ...state, loading: false, hasErrors: true };
+    default:
+      return state;
+  }
 }
